f7ab00527d465e6aec02f71e802a8a1274919292
jwang1 <sigkdd@yahoo.com>
2018-01-03 23:18:49 -0500

    Template local variable accessed via @ViewChild in typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, OnInit } from '@angular/core';
 import { LogMessage } from './shared/log-message.model';
 
 @Component({
@@ -6,7 +6,7 @@ import { LogMessage } from './shared/log-message.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'exercise3: Directives';
 
   id: number = 0;
@@ -20,6 +20,13 @@ export class AppComponent {
   childComponentValLong = '';
 
   holdTemplateLocalVarVal = '';
+
+  @ViewChild('input2')
+  templateLocalVariableViaViewChild;
+
+  valTemplateLocalVariableViaViewChild = '';
+
+  valTemplateLocalVariableViaViewChild2 = '';
 
   onToggle() {
     this.id++;
@@ -42,4 +49,13 @@ export class AppComponent {
   useTemplateLocalVariable(input: HTMLInputElement) {
     this.holdTemplateLocalVarVal = input.value;
   }
+
+  checkInput() {
+    this.valTemplateLocalVariableViaViewChild = this.templateLocalVariableViaViewChild.nativeElement.value;
+  }
+
+  ngOnInit() {
+    this.valTemplateLocalVariableViaViewChild2 = this.templateLocalVariableViaViewChild.nativeElement.value;
+  }
+
 }
